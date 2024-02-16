5033ead03274f01e015728ffc79c7c8839d00d02
Anıl <anil@Anl-MacBook-Air.local>
2024-02-16 13:15:00 +0300

    tab bar icons

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import NotificationsScreen from './screens/NotificationsScreen';
 import SessionsScreen from './screens/SessionsScreen';
+import { Entypo, Ionicons, FontAwesome5 } from '@expo/vector-icons';
 
 export default function App() {
   const Tab = createBottomTabNavigator();
@@ -14,9 +15,31 @@ export default function App() {
           tabBarActiveBackgroundColor: 'gainsboro',
         }}
       >
-        <Tab.Screen name='Main' component={MainScreen} />
-        <Tab.Screen name='Notifications' component={NotificationsScreen} />
-        <Tab.Screen name='Sessions' component={SessionsScreen} />
+        <Tab.Screen
+          name='Main'
+          component={MainScreen}
+          options={{
+            tabBarIcon: () => <Entypo name='home' size={20} color={'black'} />,
+          }}
+        />
+        <Tab.Screen
+          name='Notifications'
+          component={NotificationsScreen}
+          options={{
+            tabBarIcon: () => (
+              <Ionicons name='notifications' size={20} color={'black'} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name='Sessions'
+          component={SessionsScreen}
+          options={{
+            tabBarIcon: () => (
+              <FontAwesome5 name='receipt' color={'black'} size={20} />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- screens/FinishedScreen.js
+++ screens/FinishedScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
 import React from 'react';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 
